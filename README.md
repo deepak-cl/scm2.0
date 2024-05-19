6fb84b16d4a599180b391b322a2dda857541db12
Mighty Fellow <apgcln97@gmail.com>
2024-05-19 16:53:46 +0530

    understanding fragments

---
+++ README.md
@@ -0,0 +1,3 @@
+# Running tailwind css on watch mode
+
+npx tailwindcss -i ./src/main/resources/static/css/input.css -o ./src/main/resources/static/css/output.css --watch
---
+++ src/main/java/net/scm/scm20/controller/PageController.java
@@ -0,0 +1,36 @@
+package net.scm.scm20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class PageController {
+
+    @RequestMapping(path = "/home", method=RequestMethod.GET)  
+    public String home(Model model) {
+
+        //Sending data to view
+        model.addAttribute("name", "Youtube channel");
+        model.addAttribute("youtubeChannel", "Code decode");
+        model.addAttribute("githubRepo", "https://github.com/deepak-cl");
+        return "home";
+    }
+
+    // about page
+    @RequestMapping(path = "/about", method=RequestMethod.GET)
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", false);
+        return "about";
+    }
+    
+    // services page
+    @RequestMapping(path = "/services", method=RequestMethod.GET)
+    public String servicesPageString() {
+        return "services";
+    }
+    
+}
